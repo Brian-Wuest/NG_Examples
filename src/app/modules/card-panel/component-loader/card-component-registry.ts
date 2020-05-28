40cab90f01fee671747e9b6db53ea89a325d813d
Brian Wuest <brian.wuest@outlook.com>
2020-05-28 06:46:06 -0500

    Adding some documentation and validation messages.

--- src/app/modules/card-panel/component-loader/card-component-registry.ts
+++ src/app/modules/card-panel/component-loader/card-component-registry.ts
@@ -1,5 +1,8 @@
 import { ComponentType } from './component-type';
 
+/**
+ * This interface is used to identify a card component registration.
+ */
 export interface CardComponentRegistration {
   /**
    * The component type for this registration.
@@ -18,12 +21,40 @@ export interface CardComponentRegistration {
   initializer: Function;
 }
 
-export let CardComponentRegistry = new Array<CardComponentRegistration>();
+/**
+ * This is the registration array for card components.
+ * This is intended to be private since this is managed with the exportable functions.
+ */
+const cardComponentRegistry = new Array<CardComponentRegistration>();
 
+/**
+ *
+ * @param componentType This determines where in a card a component will be displayed.
+ * @param name The registration name of a component. If a name already exists an error will be thrown.
+ */
 export function CardComponent(componentType: ComponentType, name: string) {
+  let errorMessage = '';
+
+  if (
+    cardComponentRegistry.length > 0 &&
+    FindCardRegistration(componentType, name) !== null
+  ) {
+    errorMessage =
+      'A component registration with name: ' +
+      name +
+      ' already exists; unable to register component.';
+  }
+  else if (!componentType || !name || name === '') {
+    errorMessage = 'ComponentType or name was not supplied, unable to register component';
+  }
+
+  if (errorMessage) {
+    throw new Error(errorMessage);
+  }
+
   // tslint:disable-next-line: ban-types
   return (target: Function) => {
-    CardComponentRegistry.push({
+    cardComponentRegistry.push({
       componentType,
       name,
       initializer: target,
@@ -31,12 +62,18 @@ export function CardComponent(componentType: ComponentType, name: string) {
   };
 }
 
+/**
+ * Attempts to find a card registration by component type and/or name when supplied.
+ * Returns null if nothing was found.
+ * @param componentType The component type to look for.
+ * @param name The component registration name to look for.
+ */
 export function FindCardRegistration(
-  componentType: ComponentType,
-  name: string
+  componentType?: ComponentType,
+  name?: string
 ): CardComponentRegistration {
   if (componentType && name) {
-    for (const registration of CardComponentRegistry) {
+    for (const registration of cardComponentRegistry) {
       if (
         registration.componentType === componentType &&
         registration.name === name
@@ -45,13 +82,13 @@ export function FindCardRegistration(
       }
     }
   } else if (componentType) {
-    for (const registration of CardComponentRegistry) {
+    for (const registration of cardComponentRegistry) {
       if (registration.componentType === componentType) {
         return registration;
       }
     }
   } else if (name) {
-    for (const registration of CardComponentRegistry) {
+    for (const registration of cardComponentRegistry) {
       if (registration.name === name) {
         return registration;
       }
--- src/app/modules/card-panel/component-loader/component-type.ts
+++ src/app/modules/card-panel/component-loader/component-type.ts
@@ -1,5 +1,20 @@
+/**
+ * This enum identifies a location on a card where a component will affect.
+ * A single component can only be in a single location on a card.
+ */
 export enum ComponentType {
+  /**
+   * Indicates that the component will be displayed in the header of a card.
+   */
   Header = 'Header',
+
+  /**
+   * Indicates that the component will be displayed in the body of a card.
+   */
   Body = 'Body',
+
+  /**
+   * Indicates that the component will be in the footer of a card.
+   */
   Footer = 'Footer'
 }
