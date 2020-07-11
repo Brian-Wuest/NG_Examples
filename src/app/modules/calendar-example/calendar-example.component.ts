b0c687c8c095da83dfc142dcc883aac5847d49d8
Brian Wuest <brian.wuest@outlook.com>
2020-07-11 18:08:05 -0500

    updates to have custom styles for various things and custom event rendering

--- src/app/modules/calendar-example/calendar-example.component.ts
+++ src/app/modules/calendar-example/calendar-example.component.ts
@@ -4,6 +4,7 @@ import dayGridPlugin, { DayGridView } from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import addHours from 'date-fns/addHours';
 import addMinutes from 'date-fns/addMinutes';
+import formatDate from 'date-fns/format';
 import startOfDay from 'date-fns/startOfDay';
 import { DialogService } from 'primeng/dynamicdialog';
 import { FullCalendar } from 'primeng/fullcalendar';
@@ -81,10 +82,43 @@ export class CalendarExampleComponent implements OnInit {
         // Remove the hover class.
         info.el.classList.remove('event-hover');
       },
+      eventRender: (info: { el: HTMLElement; event: EventApi }) => {
+        // Create a custom layout for the event and then return it.
+        // This way users can see when the event starts and ends.
+        // This is the main anchor for the event.
+        var mainElement = document.createElement('A');
+        mainElement.className = 'fc-day-grid-event fc-h-event fc-event fc-start fc-end';
+
+        // Main content div for the event.
+        var containerElement = document.createElement('DIV');
+        containerElement.className = 'fc-content';
+
+        // This span contains the time-range for the event.
+        var timeElement = document.createElement('SPAN');
+        timeElement.className = 'fc-time';
+        timeElement.innerHTML = formatDate(info.event.start, 'HH:mm') + ' - ' + formatDate(info.event.end, 'HH:mm');
+        containerElement.appendChild(timeElement);
+
+        // this contains the title (summary) for the event.
+        var titleElement = document.createElement('SPAN');
+        titleElement.className = 'fc-title';
+
+        // Use a text node to show the title to avoid Cross-Site Scripting issues.
+        titleElement.appendChild(document.createTextNode(info.event.title));
+        containerElement.appendChild(titleElement);
+
+        mainElement.appendChild(containerElement);
+
+        // Return the main element as this is what is going to be attached to the DOM.
+        return mainElement;
+      },
     };
   }
 
   addEvent() {
+    // Make sure to clear out the selectedEvent field so it's not modified/removed incorrectly.
+    // Event editing is done by clicking on an event.
+    this.selectedEvent = null;
     let startDate: Date;
     let endDate: Date;
 
@@ -131,17 +165,6 @@ export class CalendarExampleComponent implements OnInit {
         this.events = [...this.events];
       }
     }
-  }
-
-  gotoDate(date: Date) {
-    this.fc.getCalendar().gotoDate(date);
-  }
-
-  addAttribute(element: HTMLElement, attributeName: string, attributeValue: string) {
-    let customAttribute = document.createAttribute(attributeName);
-
-    customAttribute.value = attributeValue;
-    element.attributes.setNamedItem(customAttribute);
   }
 
   showEventEditor(event: EventApi) {
